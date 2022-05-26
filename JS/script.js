ef9b454009b8af56705e7abc8d6ce04639f0b4c1
Kenny J <102876557+regazi@users.noreply.github.com>
2022-05-26 14:58:30 -0400

    commit on master

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,10 @@
-alert('Hello world');
+//Pokemon Statistics Array ---------------------------------------------------------
+const pokemonList = [
+    {name: 'Bulbasaur', type: ['Grass', 'Poison'], height: 0.7},
+    {name: 'Ivysaur', type: ['Grass', 'Poison'], height: 1},
+    {name: 'Venusaur', type: ['Grass', 'Poison'], height: 2},
+    {name: 'Charmander', type: ['Fire'], height: 0.6},
+    {name: 'Jigglypuff', type: ['Fairy', 'Normal'], height: 0.5},
+];
 
-let favoriteFood = 'tacos'
-document.write(favoriteFood)
+console.log(pokemonList)
