c5611caeca4496702b1858023a1ba720e4fe8c2b
Kenny J <102876557+regazi@users.noreply.github.com>
2022-05-26 16:43:56 -0400

    commit on master

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,38 @@
-//Pokemon Statistics Array ---------------------------------------------------------
-const pokemonList = [
-    {name: 'Bulbasaur', type: ['Grass', 'Poison'], height: 0.7},
-    {name: 'Ivysaur', type: ['Grass', 'Poison'], height: 1},
-    {name: 'Venusaur', type: ['Grass', 'Poison'], height: 2},
-    {name: 'Charmander', type: ['Fire'], height: 0.6},
-    {name: 'Jigglypuff', type: ['Fairy', 'Normal'], height: 0.5},
+//Pokemon Deck ---------------------------------------------------------
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        types: ['Grass', 'Poison'], 
+        height: 0.7
+    },
+    {
+        name: 'Ivysaur', 
+        types: ['Grass', 'Poison'], 
+        height: 1
+    },
+    {
+        name: 'Venusaur', 
+        types: ['Grass', 'Poison'], 
+        height: 2
+    },
+    {
+        name: 'Charmander', 
+        types: ['Fire'], 
+        height: 0.6
+    },
+    {
+        name: 'Jigglypuff', 
+        types: ['Fairy', 'Normal'], 
+        height: 0.5
+    },
 ];
+//loop that writes each pokemon to the document, with some additions for "type: Fire"
+for (let i= 0; i < pokemonList.length; i++){
+    if(pokemonList[i].types == 'Fire'){
+        document.write(pokemonList[i].name + " (" + pokemonList[i].height + "m" + ") *fire emoji<br> ") 
+    }else{
+        document.write(pokemonList[i].name + " (" + pokemonList[i].height + "m" + ") <br> ")
+    }
+}
 
-console.log(pokemonList)
+
