bbc67c24b206c35b3df0da22113590ee24e22254
Kenny J <102876557+regazi@users.noreply.github.com>
2022-05-29 10:35:27 -0400

    1)Wrapped the pokemonRepository in a function to limit access. 2) Added conditional statemment for pokemon height in the buildCard function. 3) added a filtering function and search bar (for  demonstration purposes). Search a listed pokemon by name and its info wil log to console

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,79 @@
-//Pokemon Deck ---------------------------------------------------------
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        types: ['Grass', 'Poison'], 
-        height: 0.7
-    },
-    {
-        name: 'Ivysaur', 
-        types: ['Grass', 'Poison'], 
-        height: 1
-    },
-    {
-        name: 'Venusaur', 
-        types: ['Grass', 'Poison'], 
-        height: 2
-    },
-    {
-        name: 'Charmander', 
-        types: ['Fire'], 
-        height: 0.6
-    },
-    {
-        name: 'Jigglypuff', 
-        types: ['Fairy', 'Normal'], 
-        height: 0.5
-    },
-];
-//loop that writes each pokemon to the document, with some additions for "type: Fire"
-for (let i= 0; i < pokemonList.length; i++){
-    if(pokemonList[i].types == 'Fire'){
-        document.write(pokemonList[i].name + " (" + pokemonList[i].height + "m" + ") *fire emoji<br> ") 
-    }else{
-        document.write(pokemonList[i].name + " (" + pokemonList[i].height + "m" + ") <br> ")
+
+//Pokemon Deck wrapped in IIFE with 'add' && 'get all' methods---------------------------------------------------------  
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            types: ['Grass', 'Poison'], 
+            height: 0.7
+        },
+        {
+            name: 'Ivysaur', 
+            types: ['Grass', 'Poison'], 
+            height: 1
+        },
+        {
+            name: 'Venusaur', 
+            types: ['Grass', 'Poison'], 
+            height: 2
+        },
+        {
+            name: 'Charmander', 
+            types: ['Fire'], 
+            height: 0.6
+        },
+        {
+            name: 'Jigglypuff', 
+            types: ['Fairy', 'Normal'], 
+            height: 0.5
+        },
+    ];
+  
+    function add(pokemon) {
+        if(typeof pokemon === 'object'){
+            pokemonList.push(pokemon);
+        }else{
+            return;
+        }
     }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+ 
+//loop that writes each pokemon to the document, with some additions for "type: Fire"
+    function buildCard(pokemon){
+        if(pokemon.types == 'Fire'){
+            document.write(`<p id="fire">${pokemon.name} (${pokemon.height}m) </p> <br>`) 
+        }else if(pokemon.height > 1){
+            document.write(`<p>${pokemon.name} (${pokemon.height}m) Wow, thats big!</p> <br> `)
+        }else{
+            document.write(`<p>${pokemon.name} (${pokemon.height}m) </p> <br> `)
+        }
+    }
+    pokemonRepository.getAll().forEach(buildCard); 
+//bonus task. Search functionality that returns pokemon info to the console.  
+   let searchResult = []
+   let userSearch = document.getElementById('search')
+   let submit = document.getElementById('sub')
+   submit.addEventListener('click', (e) =>{
+        e.preventDefault();
+        filterList();
+        console.log(searchResult)
+        userSearch.value = " "
+   })
+   
+   function filterList(){
+    let searchParam =  userSearch.value;
+    filteredResult = pokemonRepository.getAll().filter(pokemon => pokemon.name == searchParam);
+    searchResult.push(filteredResult);
 }
+   
 
-
+   
